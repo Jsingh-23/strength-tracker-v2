9ba389e7d54411ecab4b97ec7e9775ea0a9f3e58
Jsingh-23 <jsingh129315@gmail.com>
2023-09-08 12:48:30 -0700

    removed comments

--- utils/footballapi.js
+++ utils/footballapi.js
@@ -27,33 +27,3 @@ export async function getLeagueData() {
     throw error;
   }
 }
-
-// export async function getMultipleLeagueStandingsData() {
-//   var myHeaders = new Headers();
-//   myHeaders.append("x-rapidapi-key", process.env.NEXT_PUBLIC_FOOTBALL_API_KEY);
-//   myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
-
-//   var requestOptions = {
-//     method: 'GET',
-//     headers: myHeaders,
-//     redirect: 'follow'
-//   };
-
-//   const leagueValues = [61,135,78,39,140];
-
-//   try {
-//     const response = await fetch(`https://v3.football.api-sports.io/standings?league=${leagueValues}&season=2023`, requestOptions);
-//     if (!response.ok) {
-//       throw new Error(`HTTP Error! Status: ${response.status}`);
-//     }
-//     // console.log(process.env.NEXT_PUBLIC_FOOTBALL_API_KEY);
-//     // console.log("STATUS", response.status);
-//     const jsonData = await response.json();
-//     // console.log(jsonData);
-//     // setData(jsonData);
-//     return jsonData;
-//   } catch (error) {
-//     console.error('API Request Error:', error);
-//     throw error;
-//   }
-// }
